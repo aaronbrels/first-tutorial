fd66942270a2999326331f619e0429d69cd78db1
Aaron Brelsford <aaronbrels@gmail.com>
2021-12-27 12:17:06 -0500

    Added Footer from to the import section

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 
 import './App.css';
 import Header from './Header'
+import Content from './Content'
+import Footer from './Footer'
 
-
-function App() {  // functional component
+function App() {  // functional component, this is the parent component for header, footer, and content components that I created
 
   return (
     <div className="App">
       <Header />
-
+      <Content />
+      <Footer />
     </div>
   );
 }
